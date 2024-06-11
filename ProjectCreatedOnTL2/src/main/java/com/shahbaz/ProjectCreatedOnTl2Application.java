7fbbfd49a4ea869d71863e76874cd94fc554ea88
GitMuhammadS <mdshahbazengg@gmail.com>
2024-06-11 06:49:55 +0530

    tl proj 2

---
+++ ProjectCreatedOnTL2/src/main/java/com/shahbaz/ProjectCreatedOnTl2Application.java
@@ -0,0 +1,13 @@
+package com.shahbaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCreatedOnTl2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCreatedOnTl2Application.class, args);
+	}
+
+}
